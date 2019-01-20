1f835f23ca817f7b66cb1de27a082542f9d295b6
NicolaosJM <nicmay@gmail.com>
2019-01-19 21:41:44 -0600

    further minor updates

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friendArray = [
+    {
+        friendName: "Mio Hope",
+        friendPhoto: "https://www.catster.com/wp-content/uploads/2018/05/A-gray-cat-crying-looking-upset.jpg",
+        questionOne: 2,
+        questionTwo: 3,
+        questionThree: 2,
+        questionFour: 4,
+        questionFive: 2,
+        questionSix: 3,
+        questionSeven: 4,
+        questionEight: 3,
+        questionNine: 1,
+        questionTen: 2
+    }
+];
+
+module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var friends = require("../data/friends";)
+var friends = require("../data/friends")
 
 module.exports = function(app) {
 
